6a3c2ed3db6ddd2aae83d6f6be2f621f600c6feb
yorlysoro <yorlysoro@gmail.com>
2022-08-07 10:07:47 -0400

    timezones

---
+++ timezones.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import pytz
+
+
+my_city_timezone = pytz.timezone('America/Bogota')
+my_city_time = datetime.now(my_city_timezone)
+print("Bogota:", my_city_time.strftime("%d/%m/%Y, %H:%M:%S"))
+
+
+my_city_timezone = pytz.timezone('America/Mexico_City')
+my_city_time = datetime.now(my_city_timezone)
+print("Ciudad de México:", my_city_time.strftime("%d/%m/%Y, %H:%M:%S"))
+
+my_city_timezone = pytz.timezone('America/Caracas')
+my_city_time = datetime.now(my_city_timezone)
+print("Caracas:", my_city_time.strftime("%d/%m/%Y, %H:%M:%S"))
